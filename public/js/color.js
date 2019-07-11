88ae28b514d78b3939a6ee3c9cc2f0c25f190fe5
Erin Gurley <erinmgurley@gmail.com>
2019-07-11 11:42:26 -0700

    eslint errs

--- public/js/color.js
+++ public/js/color.js
@@ -5,29 +5,11 @@ const color = new URLSearchParams(window.location.search).get('color');
 fetch(`/api/v1/colors/${color}`)
   .then(colorContent => colorContent.json())
   .then(color => {
-    header.textContent += color.name + ' ';
-    header.textContent += color.hex + ' ';
-    header.textContent += color.r + ' ';
-    header.textContent += color.g + ' ';
-    header.textContent += color.b + ' ';
+    header.textContent += color.name + '  ';
+    header.textContent += color.hex + '  ';
+    header.textContent += color.r + '  ';
+    header.textContent += color.g + '  ';
+    header.textContent += color.b + '  ';
     header.style.color += color.name;
 
   });
-  
-
-
-
-//when making the routes
-
-// app.get('/api/v1/dogs/:whatever'), (req, res) =>
-
-// this becomes a variable and you can grab the value with 
-// console.log(req.params.whatever);
-// res.end(reg.params.whatever);
-
-// app.get('/api/v1/dogs/:name')
-// const dog = dogs.find(dog => dog.name === req.params.name)
-// res.send(dog)
-// \
-
-// just test the app test
