74b3b0dee492ead1f52afb39df1a0dfb7125954f
Erin Gurley <erinmgurley@gmail.com>
2019-07-10 17:18:34 -0700

    fixed eslint files

--- public/js/main.js
+++ public/js/main.js
@@ -6,10 +6,9 @@ fetch('/api/v1/colors')
   .then(colors => {
     colors.forEach(color => {
       const li = document.createElement('li');
-      console.log(li)
-      li.textContent = `${color.name} - ${color.hex} - ${color.r} - ${color.g} - ${color.b}`
-        colorsList.appendChild(li);
+      li.textContent = `${color.name} - ${color.hex} - ${color.r} - ${color.g} - ${color.b}`;
+      colorsList.appendChild(li);
+    });
   });
-  })
 
 root.appendChild(colorsList);
--- test/app.test.js
+++ test/app.test.js
@@ -4,9 +4,9 @@ const app = require('../lib/app');
 describe('app routes', () => {
   it('has an index page', () => {
     return request(app)
-    .get('/')
-    .then(res => {
-      expect(res.text).toEqual(expect.stringContaining('Colors'));
-    });
+      .get('/')
+      .then(res => {
+        expect(res.text).toEqual(expect.stringContaining('Colors'));
+      });
   });
 });
